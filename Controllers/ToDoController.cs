2d5beb50432d574b45498f173bf6e4dedf0b063b
jenil2209 <jenilparmar1999@gmail.com>
2023-04-13 09:26:43 +0530

    Add project files.

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{   
+    public class todocontroller : Controller
+    {
+        private readonly ToDoDb _database;
+
+        public todocontroller(ToDoDb db)
+        {
+            _database = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var todos = await _database.ToDos.ToListAsync();
+            return View(todos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Add(string newItem)
+        {
+            if (newItem != null)
+            {
+                var todo = new Todoitem
+                {
+                    Name = newItem,
+                    IsDone = false
+                };
+                _database.ToDos.Add(todo);
+                await _database.SaveChangesAsync();
+            }
+
+            return RedirectToAction("ToDo");
+        }
+
+        public IActionResult Complete(IEnumerable<int> isComplete)
+        {
+            if (isComplete != null)
+            {
+                foreach (var item in isComplete)
+                {
+                    var todo = _database.ToDos.FirstOrDefault(t => t.Id == item);
+                    if (todo != null)
+                    {
+                        todo.IsDone = true;
+                    }
+                }
+                _database.SaveChanges();
+            }
+            return RedirectToAction("ToDo");
+        }
+
+        public IActionResult DeleteAll()
+        {
+            _database.ToDos.RemoveRange(_database.ToDos);
+            _database.SaveChanges();
+            return RedirectToAction("ToDo");
+        }
+
+    }
+}
---
+++ Data/ToDoDb.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+using Microsoft.Data.Sqlite;
+
+namespace WebApplication2.Data
+{
+    public class ToDoDb : DbContext
+    {
+        public string DbPath { get; }
+
+        public ToDoDb(DbContextOptions<ToDoDb> options) : base(options)
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+        public DbSet<Todoitem> ToDos { get; set; }
+
+    }
+}
